756b2f38e1a99f350189d73803781bd7ece8f360
Tan Nguyen <tantanb2@gmail.com>
2018-04-19 10:36:49 +0700

    fix conflict

--- app/helpers/Function.php
+++ app/helpers/Function.php
@@ -313,6 +313,7 @@ function convertToHoursMins($time, $format = '%02d:%02d') {
     $hours = floor($time / 60);
     $minutes = ($time % 60);
     return sprintf($format, $hours, $minutes);
+}
 
 function checkPermissionBySlug($slug)
 {
