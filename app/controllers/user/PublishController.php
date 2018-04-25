9e850905320f4540ecf5a8058f9d4c0b5868c52c
tungtt2 <trantunghn196@gmail.com>
2018-04-25 23:45:34 +0700

    fix

--- app/controllers/user/PublishController.php
+++ app/controllers/user/PublishController.php
@@ -82,7 +82,7 @@ class PublishController extends AdminController {
         //tao moi trong bang notification
         $gmo = Admin::find($teacher->admin_id);
         $title = 'Giáo viên có email '.$teacher->email.' vừa nhận học sinh';
-        $message = '<a href="/waitting_gmo"> '.$teacher->full_name. 'nhận học sinh'. $student->full_name.'</a>';
+        $message = '<a href="/admin/waitting_gmo"> '.$teacher->full_name. 'nhận học sinh'. $student->full_name.'</a>';
         if ($gmo) {
             Notification::create([
                 'sender_model' => 'Teacher',
