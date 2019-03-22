9656f92c0e7eda2a9a239a60dfdd7c81f1834f34
tungtt2 <trantunghn196@gmail.com>
2019-03-22 22:17:11 +0700

    fix

--- app/views/hung/thong-bao.blade.php
+++ app/views/hung/thong-bao.blade.php
@@ -73,14 +73,23 @@ var dataVin = '" [ {\n        \"name\": \"' +name +
     '\",\n                \"weight\": \"100\",\n                \"discount_percent_product\": \"0\",\n                \"discount_amount_product\": \"0\",\n                \"tax1\": \"0\",\n                \"qtyinstock\": \"0\"\n            }\n        \n    ]\n}]\n"'
 ;
     
-$.ajax({
-    url : "https://new.abit.vn/invoices/createInvoiceFromPartner/2/vinstore/Zxb9yNn2TC5fTXL",
-    type : "post",
-    dataType:"json",
-    data : dataVin,
-    success : function (result){
-        console.log(11);
-    }
+console.log(dataVin);
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://new.abit.vn/invoices/createInvoiceFromPartner/2/vinstore/Zxb9yNn2TC5fTXL",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+  },
+  "processData": false,
+  "data": '"[ {\n\t\"name\": \"tung tran t111hanh\",\"'+telephone+'\": \"0123456789\",\n    \"name_receiver\": \"Mr Du\",\n    \"invoicestatus\": \"AutoCreated\",\n    \"city\": \"\",\n    \"district\": \"\",\n    \"address\": \"203 Minh Khai\",\n    \"note\": \"\",\n    \"hinhthucvc\": \"Chuyenthuong\",\n    \"discount_percent\": \"0.000\",\n    \"discount_amount\": \"0.000\",\n    \"s_h_amount\": \"0.000\",\n    \"ghichu1\": \"\",\n    \"ghichu2\": \"\",\n    \"notevanchuyen\": \"\",\n    \"giamgia\": \"0\",\n    \"phuthu\": \"0\",\n    \"phivanchuyen\": \"0\",\n    \"adjustment\": \"0.000\",\n    \"taxtype\": \"individual\",\n    \"deposits\": 0,\n    \"subtotal\": 450000,\n    \"taxtotal_invoice\": \"0.000\",\n    \"total\": 450000,\n    \"accountid\": 0,\n    \"list_pageitem\": \"289582808317941\",\n    \"listProduct\": [\n        {\n            \"price\": \"250000\",\n            \"amount\": \"2\",\n            \"productName\": \"SP Xoay 3 vòng\",\n            \"productcode\": \"xoay-3-vong11\",\n            \"weight\": \"100\",\n            \"discount_percent_product\": \"0\",\n            \"discount_amount_product\": \"0\",\n            \"tax1\": \"0\",\n            \"qtyinstock\": \"0\"\n        }\n        \n    ]\n}]\n"'
+}
+
+console.log('---------');
+console.log(settings);
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
 </script>
 @include('hung.footer')
